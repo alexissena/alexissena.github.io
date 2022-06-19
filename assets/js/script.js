198861dea4880a8b90a3a433e3475272b05b9c60
alexissena <alexissena06@gmail.com>
2022-06-18 19:52:58 -0500

    cambios al archico jz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,172 +1,220 @@
+(function ($) {
+  'use strict';
 
-let usuarios;
-var $table = $('#tblResultados');
-let usuariosAgregados = [];
+  // Preloader
+  $(window).on('load', function () {
+    $('#preloader').fadeOut('slow', function () {
+      $(this).remove();
+    });
+  });
 
-$("#buscar").on("click", function () {
-    let url = "https://randomuser.me/api/?results=10";
-    fetch(url)
-        .then((response) => response.json())
-        .then((respuesta) => {
-            console.log(respuesta)
-            usuarios = respuesta.results;
-            console.log(usuarios);
 
-            $table.bootstrapTable({ data: usuarios });
+  // Instagram Feed
+  if (($('#instafeed').length) !== 0) {
+    var accessToken = $('#instafeed').attr('data-accessToken');
+    var userFeed = new Instafeed({
+      get: 'user',
+      resolution: 'low_resolution',
+      accessToken: accessToken,
+      template: '<a href="{{link}}"><img src="{{image}}" alt="instagram-image"></a>'
+    });
+    userFeed.run();
+  }
 
+  setTimeout(function () {
+    $('.instagram-slider').slick({
+      dots: false,
+      speed: 300,
+      // autoplay: true,
+      arrows: false,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
         }
-        )
-        .catch(console.error())
-});
-
-
-function nameFormatter(value, row) {
-    //console.log(value)
-    //console.log(row)
-    return value.first + " " + value.last;
-}
-
-
-function cargarSelect(data) {
-
-    let arrUnicos = removeDuplicates(data, "nat");
-    let arrNat = [];
-    arrUnicos.forEach(element => {
-        arrNat.push(element.nat);
-    });
-    $(".selectBootstrap").select2({
-        data: arrNat
-    })
-
-}
-
-function removeDuplicates(originalArray, prop) {
-    var newArray = [];
-    var lookupObject = {};
-
-    for (var i in originalArray) {
-        lookupObject[originalArray[i][prop]] = originalArray[i];
-    }
-
-    for (i in lookupObject) {
-        newArray.push(lookupObject[i]);
-    }
-    return newArray;
-}
-
-$('#opciones').on('change', function () {
-    let filtro = this.value;
-    let filteredValue = usuarios.filter(function (item) {
-        return item.nat == filtro;
-    });
-    $table.bootstrapTable('removeAll')
-    $table.bootstrapTable('load', filteredValue)
-
-});
-
-
-window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-        usuariosAgregados.push(row);
-    },
-}
-function removeItemFromArr(arr, item) {
-    return arr.filter(function (e) {
-        return e !== item;
-    });
-};
-
-
-
-$("#verDatos").on("click", function () {
-    const mostrardatos = (data) => {
-        let datos = '';
-
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i].gender == "male");
-
-            datos += `
-            <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="image-flip">
-                <div class="mainflip flip-0">
-                    <div class="frontside">
-                        <div class= `
-            if (usuarios[i].gender == "male") {
-                datos += '"male"'
-            } else {
-                datos += '"female"'
-            }
-            datos += `
-                >
-                <div class="card-body text-center">
-                <p>
-                    <img class=" img-fluid radius" src="${data[i].picture.large}" alt="card image">
-                </p>
-                <h4 class="card-title">${data[i].name.title} ${data[i].name.first} ${data[i].name.last}</h4>
-                <p class="card-text">Email: ${data[i].email}<br> Pais: ${data[i].location.country}<br> Ciudad: ${data[i].location.city}</p>
-                <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm">
-                    <i class="fa fa-plus"></i>
-                </a>
-                </div>
-                </div>
-                </div>
-                <div class="backside">
-                <div class= `
-            if (usuarios[i].gender == "male") {
-                datos += '"male"'
-            } else {
-                datos += '"female"'
-            }
-            datos += `
-                >
-            <div class="card-body text-center mt-4">
-            <h4 class="card-title">${data[i].gender}</h4>
-            <p class="card-text">${data[i].cell}
-                <br>"${data[i].nat} 
-            </p>
-            <ul class="list-inline">
-                <li class="list-inline-item">
-                    <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
-                        <i class="fa fa-facebook"></i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
-                        <i class="fa fa-twitter"></i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
-                        <i class="fa fa-skype"></i>
-                    </a>
-                </li>
-                <li class="list-inline-item">
-                    <a class="social-icon text-xs-center" target="_blank" href="https://www.fiverr.com/share/qb8D02">
-                        <i class="fa fa-google"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-</div>   
-            `
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3
         }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+      ]
+    });
+  }, 1500);
 
-        $("#teams").html(datos);
-    }
-    mostrardatos(usuarios);
 
-});
+  // e-commerce touchspin
+  $('input[name=\'product-quantity\']').TouchSpin();
 
-$("#resetear").on("click", function () {
-    $("#teams").html("");
-    usuariosAgregados = [];
 
-});
+  // Video Lightbox
+  $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+
+  // Count Down JS
+  $('#simple-timer').syotimer({
+    year: 2022,
+    month: 5,
+    day: 9,
+    hour: 20,
+    minute: 30
+  });
+
+  //Hero Slider
+  $('.hero-slider').slick({
+    // autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+    dots: true,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  $('.hero-slider').slickAnimation();
+
+
+})(jQuery);
 
 $(document).ready(function () {
-    $('.selectBootstrap').select2();
+  $("#productos").text("");
+  cargarProductos();
 });
+let productos = [];
+function cargarProductos() {
+  axios({
+    method: 'get',
+    url: 'data/productos.json'
+  })
+    .then(function (response) {
+      // console.log(response);
+      if (response.status === 200) {
+        productos = response.data;
+        response.data.forEach(element => {
+          let promo = "";
+          let moneda = "S/.";
+          let valor = 0.00;
+          if (element.promocion)
+            promo = '<span class="bage">En Promocion</span>'
+          if (tipoCambio === "Soles") {
+            moneda = "S/.";
+          }
+          switch (tipoCambio) {
+            case "Soles":
+              moneda = "S/.";
+              valor = element.valor
+              break;
+            case "Dolares":
+              moneda = "$";
+              valor = Math.round(element.valor / 3.77)
+              break;
+            case "Euros":
+              moneda = "€"
+              valor = Math.round(element.valor / 3.94)
+
+              break;
+            default:
+              moneda = "S/.";
+              break;
+          }
+          $("#productos").append('<div class="col-md-4"><div class="product-item fondo borde" ><div class="product-thumb">'
+            + promo + '<img class="img-responsive" src="'
+            + element.imagen + ' " alt="product-img"/><div class="preview-meta"><ul><li><span data-toggle="modal" data-target="#product-modal" onclick="productoActual(\''
+            + element.codigo + '\')"><i class="tf-ion-ios-search-strong"></i></span></li><li> <a href="#!" ><i class="tf-ion-ios-heart"></i></a></li><li><a href="#!"><i class="tf-ion-android-cart"></i></a></li></ul> </div></div><div class="product-content"><h4><a href="product-single.html">'
+            + element.nombre + '</a></h4><p class="price">'
+            + moneda
+            + valor + '</p></div></div>');
+        });
+
+      }
+      else {
+        console.log("No se ejecuto")
+      }
+    });
+}
+let tipoCambio = 'Soles';
+$('#tipoCambio').change(function () {
+  tipoCambio = $(this).val();
+  $("#productos").text("");
+  cargarProductos();
+  cargarCarrito();
+});
+
+let itemActual;
+let carritoCompras = [];
+
+function productoActual(id) {
+  let item = productos.find(item => item.codigo === id)
+  itemActual = item;
+  $("#imgProducto").attr("src", item.imagen);
+  $("#nombreProducto").text(item.nombre);
+  $("#precioProducto").text(item.valor);
+  $("#DescripcionProducto").text(item.descripcion);
+}
+
+$("#agregarItem").on("click", function () {
+  carritoCompras.push(itemActual);
+  cargarCarrito();
+});
+
+let totalCarrito = 0;
+
+function cargarCarrito() {
+  cantidadProductos = 0;
+  totalCarrito = 0
+
+  $("#carrito").text("");
+
+  let cargarCarrito = [...new Set(carritoCompras)];
+  console.log(cargarCarrito);
+
+  cargarCarrito.forEach(element => {
+    let cantidadProductos = carritoCompras.reduce((acumulador, itemID) => {
+      return element.codigo === itemID.codigo ? acumulador += 1 : acumulador;
+    }, 0);
+    console.log(cantidadProductos);
+    if (tipoCambio === "Soles") {
+      moneda = "S/.";
+    }
+    switch (tipoCambio) {
+      case "Soles":
+        moneda = "S/.";
+        valor = element.valor
+        break;
+      case "Dolares":
+        moneda = "$";
+        valor = Math.round(element.valor / 3.77)
+        break;
+      case "Euros":
+        moneda = "€"
+        valor = Math.round(element.valor / 3.94)
+        break;
+      default:
+        moneda = "S/.";
+        break;
+    }
+    totalCantidad = cantidadProductos * valor;
+
+    $("#carrito").append('<div class="media"><a class="pull-left" href="#!"><img class="media-object" src="'
+      + element.imagen + '" alt="image"/></a><div class="media-body"><h4 class="media-heading"><a href="#!">'
+      + element.nombre + '</a></h4><div class="cart-price"><span>' + cantidadProductos + '</span> x <span>'
+      + moneda + totalCantidad + '</span></div><h5><strong>'
+      + moneda + totalCantidad + '</strong></h5></div><a href="#!" class="remove"><i class="tf-ion-close"></i></a></div>');
+    totalCarrito += totalCantidad;
+  })
+
+  $("#totalCarrito").text((moneda) + totalCarrito);
+}
